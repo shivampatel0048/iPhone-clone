e472b05e802f005bd5a69aef9d4c37fb7372709b
Shivam Patel <shivampatel0048@gmail.com>
2024-03-25 22:04:43 +0530

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import { Html } from '@react-three/drei'
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <Html>
+            <span class="loader"></span>
+        </Html>
+    )
+}
+
+export default Loader
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://0ea7b48c3dfb34bba885c0ee74601cc1@o4506971639054336.ingest.us.sentry.io/4506971645739008",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
